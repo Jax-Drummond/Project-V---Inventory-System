7761cb00cfe00881ede1ae05620ed03e4271727c
Jax <jaxdrummond9@gmail.com>
2025-09-24 19:37:35 -0400

    Reorganized project to fit stack

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -64,54 +64,6 @@ class ProductController
         }
     }
 
-    static async createProduct(req, res)
-    {
-        try
-        {
-            const product = await Product.create(req.body)
-            res.status(201).json(product)
-        }
-        catch (e)
-        {
-            res.status(400).json({error: e.message})
-        }
-    }
-
-    static async updateProduct(req, res)
-    {
-        try
-        {
-            const product = await Product.findByPk(req.params.id)
-            if (!product)
-            {
-                return res.status(404).json({message: "Product not found."})
-            }
-            await product.update(req.body)
-            res.status(200).json(product)
-        }
-        catch (e)
-        {
-            res.status(400).json({error: e.message})
-        }
-    }
-
-      static async deleteProduct(req, res)
-    {
-        try
-        {
-            const product = await Product.findByPk(req.params.id)
-            if (!product)
-            {
-                return res.status(404).json({message: "Product not found."})
-            }
-            await product.destroy()
-            res.status(200).json({message: "Product Deleted."})
-        }
-        catch (e)
-        {
-            res.status(400).json({error: e.message})
-        }
-    }
 }
 
 export default ProductController
